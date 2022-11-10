be57e61eb0f5cdfb82648a9ebb776eb39a6e731e
Laurence Bank <laurencebank@Laurences-Air.home>
2022-11-10 20:35:55 +0000

    modified to compile as pure C on SDCC (MCS51)

--- src/TIFF_G4.h
+++ src/TIFF_G4.h
@@ -1,15 +1,3 @@
-// Copyright 2020 BitBank Software, Inc. All Rights Reserved.
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//    http://www.apache.org/licenses/LICENSE-2.0
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-//===========================================================================
-
 #ifndef __TIFFG4__
 #define __TIFFG4__
 #if defined( __MACH__ ) || defined( __LINUX__ )
@@ -23,11 +11,18 @@
 #define pgm_read_word(s) *(int16_t *)s
 #else
 #include <Arduino.h>
-#ifndef PROGMEM
+#if !defined( PROGMEM ) || defined(ARDUINO_ARCH_MCS51)
 #define PROGMEM
 #define memcpy_P memcpy
+#ifdef ARDUINO_ARCH_MCS51
+#define REENTRANT __reentrant
+#else
+#define REENTRANT  
+#endif // ARDUINO_ARCH_MCS51
 #define pgm_read_byte(p) *(uint8_t *)p
 #define pgm_read_word(p) *(uint16_t *)p
+#else
+#define REENTRANT  
 #endif
 #endif
 //
@@ -111,11 +106,11 @@ typedef struct tiff_draw_tag
 } TIFFDRAW;
 
 // Callback function prototypes
-typedef int32_t (TIFF_READ_CALLBACK)(TIFFFILE *pFile, uint8_t *pBuf, int32_t iLen);
-typedef int32_t (TIFF_SEEK_CALLBACK)(TIFFFILE *pFile, int32_t iPosition);
-typedef void (TIFF_DRAW_CALLBACK)(TIFFDRAW *pDraw);
-typedef void * (TIFF_OPEN_CALLBACK)(const char *szFilename, int32_t *pFileSize);
-typedef void (TIFF_CLOSE_CALLBACK)(void *pHandle);
+typedef int32_t (TIFF_READ_CALLBACK)(TIFFFILE *pFile, uint8_t *pBuf, int32_t iLen) REENTRANT;
+typedef int32_t (TIFF_SEEK_CALLBACK)(TIFFFILE *pFile, int32_t iPosition) REENTRANT;
+typedef void (TIFF_DRAW_CALLBACK)(TIFFDRAW *pDraw) REENTRANT;
+typedef void * (TIFF_OPEN_CALLBACK)(const char *szFilename, int32_t *pFileSize) REENTRANT;
+typedef void (TIFF_CLOSE_CALLBACK)(void *pHandle) REENTRANT;
 
 //
 // our private structure to hold a TIFF image decode state
